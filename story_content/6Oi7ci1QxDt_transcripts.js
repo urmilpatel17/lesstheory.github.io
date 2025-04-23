3bf9a470bdfb05f20f99f75a69abf8a6300dcbee
Urmil Patel <urmilpatel17@users.noreply.github.com>
2025-04-23 16:10:38 -0400

    patel_loblaw
    added storyline file

---
+++ story_content/6Oi7ci1QxDt_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":0,"text":"Our purpose at Loblaw goes beyond\r\nbusiness. "},{"start":3086,"text":"It's about helping Canadians live life well. "},{"start":6157,"text":"Think of trust as the\r\nsummit we're all climbing toward."},{"start":9092,"text":"We build this trust through our core\r\nvalues forming our foundation,"},{"start":12850,"text":"blue culture principles guiding our path,"},{"start":15535,"text":"and thoughtful decision-making helping us reach the peak."},{"start":19045,"text":"When these elements\r\nwork together,"},{"start":20901,"text":"we create an environment where trust\r\nnaturally flourishes."}]}]};
+    window.globalLoadJsAsset('story_content/6Oi7ci1QxDt_transcripts.js', JSON.stringify(data));
+})();

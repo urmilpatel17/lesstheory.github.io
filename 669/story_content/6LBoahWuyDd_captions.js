671008a29228b5faa83c8985121f53d6648d7e7a
Urmil Patel <urmilpatel17@users.noreply.github.com>
2025-04-23 16:46:53 -0400

    new upload
    similar to liz's github interface

---
+++ 669/story_content/6LBoahWuyDd_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.98.34263.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:01.777%0D%0AClick%20on%20the%20cross%20to%20go%20back.%0D%0A%0D%0A"}')
